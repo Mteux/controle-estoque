c6f6bb8578f5404af8ff54effde3914d5740221a
Mateus <smateus2605@gmail.com>
2024-08-29 22:12:21 -0300

    manutenção no filtro

--- public/script/script.js
+++ public/script/script.js
@@ -1,3 +1,9 @@
-filtro() {
-    
+function filtro() {
+  document.getElementById("produto").style.display = "none";
+  document.getElementById("produto").innerHTML = "<select>"
+}
+
+
+function ocultarTh() {
+  
 }
