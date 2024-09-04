1752556ceae182ab4fb53790848f8b96fa478b2f
Mateus <smateus2605@gmail.com>
2024-09-03 21:53:45 -0300

    conclusao da estilização do filtro

--- public/script/script.js
+++ public/script/script.js
@@ -1,4 +1,8 @@
 function filtrar() {
-  document.getElementById("filtro").style.display = "";
-  console.log("ok");
+  var modFiltro = document.getElementById("form-filtro");
+  if (modFiltro.style.display == "none") {
+    modFiltro.style.display = "block";
+  } else {
+    modFiltro.style.display = "none";
+  }
 }
