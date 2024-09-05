bb876d3873bff1998952a6ef42898261758fdb06
Mateus <smateus2605@gmail.com>
2024-09-04 22:10:04 -0300

    adicionando as funcionalidades do filtro

--- public/script/script.js
+++ public/script/script.js
@@ -1,8 +1,36 @@
 function filtrar() {
-  var modFiltro = document.getElementById("form-filtro");
+  let modFiltro = document.getElementById("form-filtro");
   if (modFiltro.style.display == "none") {
     modFiltro.style.display = "block";
   } else {
     modFiltro.style.display = "none";
   }
+  let buttonPesquisar = document.getElementById("pesquisar");
+  pesquisar(buttonPesquisar);
+}
+
+function pesquisar(botao) {
+  botao.onclick = function () {
+    let produto = document.getElementById("filtro-produto").value;
+    let categoria = document.getElementById("filtro-categoria").value;
+    let vlrMaximo = document.getElementById("vlrMaximo").value;
+    let vlrMinimo = document.getElementById("vlrMinimo").value;
+    let quantidade = document.getElementById("quantidade").value;
+
+    let tabela = document.getElementById("tabela");
+
+    let linhas = Array.from(tabela.querySelectorAll("tbody tr"));
+
+    linhas.forEach(function (linha) {
+      let celulas = Array.from(linha.querySelectorAll("td"));
+      let prodTable = celulas[0].innerText;
+      let valorTable = celulas[1].innerText;
+      let quantTable = celulas[2].innerText;
+      let cateTable = celulas[3].innerText;
+
+      console.log(
+        `celulas: ${celulas[1].innerText} Produto : ${prodTable} , valor : ${valorTable}, quantidade: ${quantTable}, categoria: ${cateTable}`
+      );
+    });
+  };
 }
